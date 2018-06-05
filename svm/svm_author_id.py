14b6e7267cfd900eb61a0107d9669ec5ecfdfb30
Amr M. Kayid <amrkayid2027@gmail.com>
2018-06-05 07:21:12 +0200

    Finish SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=10000.0)
+
+# These lines effectively slice the training dataset down to 1% of its original size, tossing out 99% of the training data
+features_train = features_train[:len(features_train)//100] 
+labels_train = labels_train[:len(labels_train)//100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print("testing time:", round(time()-t1, 3), "s")
+
+print("Pred 10: ", pred[10])
+print("Pred 26: ", pred[26])
+print("Pred 50: ", pred[50])
+
+# Count for Chris (1) class
+c = 0
+for i in pred:
+    if i == 1:
+        c += 1
+
+print("Chris emails: ", c)
+
+print(clf.score(features_test, labels_test))
+
 #########################################################
 
 
