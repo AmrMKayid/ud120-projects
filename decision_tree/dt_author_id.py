a084639f49f1dc6e07c65ed8455787894fcb0be4
Amr M. Kayid <amrkayid2027@gmail.com>
2018-06-14 04:15:51 +0200

    Finish DT

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print("# of Feature:", len(features_train[0]))
+
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print("testing time:", round(time()-t1, 3), "s")
+
+print(clf.score(features_test, labels_test))
 
 #########################################################
 
